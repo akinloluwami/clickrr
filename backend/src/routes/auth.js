c3c0f92030bc677dd34204ceaa9c29d68367ec5e
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-06-24 04:36:22 +0100

    add verify OTP

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,5 +2,5 @@ const auth = require("../controllers/auth");
 const router = require("express").Router();
 
 router.post("/signup", auth.signup);
-
+router.post("/verifyotp", auth.verifyOTP);
 module.exports = router;

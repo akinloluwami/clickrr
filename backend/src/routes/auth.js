219a8bb2c762b20533704f3453ca4800e86a7967
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-06-24 22:00:19 +0100

    add login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,4 +3,5 @@ const router = require("express").Router();
 
 router.post("/signup", auth.signup);
 router.post("/verifyotp", auth.verifyOTP);
+router.post("/login", auth.login);
 module.exports = router;

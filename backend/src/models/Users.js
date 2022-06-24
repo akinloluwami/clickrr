a741ccf184ed02e3a3cb41bd5e9e0c2fd7f587a1
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-06-24 04:21:05 +0100

    add 'lasLogin' to user schema

--- backend/src/models/Users.js
+++ backend/src/models/Users.js
@@ -68,6 +68,10 @@ const userSchema = new Schema({
     type: Boolean,
     default: true,
   },
+  lastLogin: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);

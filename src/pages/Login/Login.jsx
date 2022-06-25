93dc4d54e67d8946acf0ed9183e23a052cd2d999
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-06-25 07:49:48 +0100

    add login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="topbar">
+        <div className="logo">
+          <h1>Clickrr</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
